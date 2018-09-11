e34359082ad5aff449cee04af02ae9f638f4db8f
Faradey27 <andriit@wix.com>
2018-09-11 17:26:37 +0300

    Add e2e test for popover prop for Tooltip

--- src/Tooltip/Tooltip.e2e.js
+++ src/Tooltip/Tooltip.e2e.js
@@ -37,6 +37,19 @@ describe('Tooltip', () => {
             });
         });
     });
+
+    eyes.it('should show tooltip in popover mode', async () => {
+      const EC = protractor.ExpectedConditions;
+      const driver = tooltipTestkitFactory({dataHook});
+      await driver.togglePopover();
+      eyes.checkWindow('Check tooltip with popover styles');
+      await waitForVisibilityOf(driver.getTooltipContentElement(tooltipDataHook), 'Cannot find Tooltip content');
+      await driver.clickOn('tooltip-anchor');
+      await browser.wait(EC.not(EC.presenceOf(driver.getTooltipContentElement(tooltipDataHook))));
+      eyes.checkWindow('Check that we do not have tooltip content');
+      await driver.clickOn('tooltip-anchor');
+      await waitForVisibilityOf(driver.getTooltipContentElement(tooltipDataHook), 'Cannot find Tooltip content');
+    });
   });
 
   describe('Popover story', () => {
