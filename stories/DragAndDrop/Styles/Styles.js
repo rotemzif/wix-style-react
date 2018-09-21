faccd623d3f10704e72f2c2df2558c952caceebd
Faradey27 <andriit@wix.com>
2018-09-21 18:07:16 +0300

    Update styles docs

--- stories/DragAndDrop/Styles/Styles.js
+++ stories/DragAndDrop/Styles/Styles.js
@@ -1,23 +1,11 @@
 import React from 'react';
 
-import CodeExample from 'wix-storybook-utils/CodeExample';
 import Markdown from 'wix-storybook-utils/Markdown';
 
 import Heading from 'wix-style-react/Heading';
 import Badge from 'wix-style-react/Badge';
 
 import Styles from './Styles.md';
-import StylesExample from './StylesExample';
-import StylesExampleRaw from '!raw-loader!./StylesExample';
-import StylesExampleScssRaw from '!raw-loader!./StylesExample.scss';
-
-const StylesExampleRawCombined = `
-//StylesExample.js
-${StylesExampleRaw}
-
-//StylesExample.scss
-${StylesExampleScssRaw}
-`;
 
 export default () => (
   <div>
@@ -25,8 +13,5 @@ export default () => (
       Styles <Badge skin="danger">Under development</Badge>
     </Heading>
     <Markdown source={Styles}/>
-    <CodeExample title="Sortable List - Single Area" code={StylesExampleRawCombined}>
-      <StylesExample/>
-    </CodeExample>
   </div>
 );
