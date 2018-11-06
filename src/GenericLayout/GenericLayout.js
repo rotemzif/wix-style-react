7a2ec6030d143cdebe8076d5673c23051cd0ea61
Aleksandr Davydov <aleksandrd@wix.com>
2018-11-06 12:17:58 +0200

    Generic layout

---
+++ src/GenericLayout/GenericLayout.js
@@ -0,0 +1,45 @@
+import classNames from 'classnames';
+import PropTypes from 'prop-types';
+import React from 'react';
+
+import WixComponent from '../BaseComponents/WixComponent';
+
+import styles from './GenericLayout.scss';
+
+
+class GenericLayout extends WixComponent {
+  render() {
+    const {
+      fullscreen,
+            header,
+            content,
+            footer
+          } = this.props;
+
+    const containerClassNames = classNames(
+      styles.container,
+      {
+        [styles.fullscreenContainer]: fullscreen
+      }
+    );
+
+    return (
+      <div className={containerClassNames}>
+        <div>{header}</div>
+
+        <div className={styles.content}>{content}</div>
+
+        <div>{footer}</div>
+      </div>
+    );
+  }
+}
+
+GenericLayout.propTypes = {
+  header: PropTypes.node,
+  content: PropTypes.node,
+  footer: PropTypes.node,
+  fullscreen: PropTypes.bool
+};
+
+export default GenericLayout;
