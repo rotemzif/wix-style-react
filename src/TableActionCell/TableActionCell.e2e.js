13a32abc87430286b7cafe887a9b048b87ef440a
Yarden Sod-Moriah <yardnsm@gmail.com>
2018-11-06 11:29:50 +0200

    <TableActionCell/> - update e2e tests to also check with the PopoverMenu opened

--- src/TableActionCell/TableActionCell.e2e.js
+++ src/TableActionCell/TableActionCell.e2e.js
@@ -2,22 +2,35 @@ import eyes from 'eyes.it';
 import {createStoryUrl, waitForVisibilityOf, scrollToElement} from 'wix-ui-test-utils/protractor';
 
 import {storySettings} from '../../stories/TableActionCell/storySettings';
+import {tableActionCellTestkitFactory} from '../../testkit/protractor';
 
-const byDataHook = dataHook => $(`[data-hook="${dataHook}"]`);
 const hoverElement = element => browser.actions().mouseMove(element).perform();
 
 describe('Table Action Cell', () => {
   const storyUrl = createStoryUrl({kind: storySettings.kind, story: storySettings.storyName});
 
   const verifyItem = async dataHook => {
-    const element = byDataHook(dataHook);
+    const driver = tableActionCellTestkitFactory({dataHook});
+    const element = driver.element();
 
     await waitForVisibilityOf(element, `Cannot find ${dataHook}`);
     await scrollToElement(element);
 
-    await eyes.checkWindow(dataHook);
+    // Check idle
+    await eyes.checkWindow(`${dataHook} idle`);
+
+    // Check hovered
     hoverElement(element);
-    await eyes.checkWindow(dataHook);
+    await eyes.checkWindow(`${dataHook} hovered`);
+
+    // Check with PopoverMenu opened; It may not be present in all of the
+    // examples, hence the try/catch block.
+    try {
+      await driver.getHiddenActionsPopoverMenu().click();
+      await eyes.checkWindow(`${dataHook} PopoverMenu opened`);
+    } catch (e) {
+      // ignored
+    }
   };
 
   const examples = {
@@ -25,6 +38,7 @@ describe('Table Action Cell', () => {
     'White primary action': 'story-primary-white',
     'Primary and secondary actions': 'story-primary-secondary',
     'Primary and hidden secondary actions': 'story-primary-hidden-secondary',
+    'With custom PopoverMenu props': 'story-popover-menu-props',
     'Always visible secondary actions': 'story-always-visible-secondary',
     'Only secondary actions': 'story-only-secondary',
     'Only visible secondary actions': 'story-only-visible-secondary',
