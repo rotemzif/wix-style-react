4197252c44cde96afde4acb70634803c24767aa7
Yarden Sod-Moriah <yardnsm@gmail.com>
2018-11-06 10:58:30 +0200

    <TableActionCell/> - added popoverMenuProps

---
+++ stories/TableActionCell/examples/PopoverMenuPropsExample.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import TableActionCell from 'wix-style-react/TableActionCell';
+import {Star, Download, Duplicate, Print} from 'wix-style-react/new-icons';
+
+import style from '../TableActionCell.story.st.css';
+
+const Example = () => (
+  <div className={style.exampleRow}>
+    <TableActionCell
+      dataHook="story-popover-menu-props"
+      primaryAction={{
+        text: 'Edit',
+        theme: 'fullblue',
+        onClick: () => window.alert('Primary action was triggered!')
+      }}
+      secondaryActions={[
+        {text: 'Star', icon: <Star/>, onClick: () => window.alert('Star action was triggered.')},
+        {text: 'Download', icon: <Download/>, onClick: () => window.alert('Download action was triggered.')},
+        {text: 'Duplicate', icon: <Duplicate/>, onClick: () => window.alert('Duplicate action was triggered.')},
+        {text: 'Print', icon: <Print/>, onClick: () => window.alert('Print action was triggered.')}
+      ]}
+      numOfVisibleSecondaryActions={0}
+      popoverMenuProps={{
+        placement: 'bottom'
+      }}
+      />
+  </div>
+);
+
+export default Example;
