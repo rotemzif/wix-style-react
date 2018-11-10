24971ede182a1071ab93220c6cdbe2853546a5dd
argshook <argshook@gmail.com>
2018-11-10 19:33:08 +0200

    test(Collapse): add check for inner component data hook

--- src/Collapse/Collapse.spec.js
+++ src/Collapse/Collapse.spec.js
@@ -5,23 +5,31 @@ import Collapse from './';
 
 describe('Collapse', () => {
   it('should render children', () => {
-    const wrapper = mount(<Collapse children="hello"/>);
+    const wrapper = mount(<Collapse children="hello" />);
     expect(wrapper.text()).toEqual('hello');
   });
 
   describe('`open` prop', () => {
     it('should not render children when false', () => {
-      const wrapper = mount(<Collapse open={false} children="hello"/>);
+      const wrapper = mount(<Collapse open={false} children="hello" />);
       expect(wrapper.children().text()).toEqual(null);
     });
   });
 
   describe('`dataHook` prop', () => {
     it('should be passed to children', () => {
+      const hookForRoot = "I'm Hooked!";
+      const hookOfChild = 'Leave britney alone!';
+
       const wrapper = mount(
-        <Collapse dataHook="I'm hooked!" children={<div>text</div>}/>
+        <Collapse
+          dataHook={hookForRoot}
+          children={<div data-hook={hookOfChild}>arbitrary content</div>}
+        />
       );
-      expect(wrapper.children().prop('data-hook')).toEqual('I\'m hooked!');
+
+      expect(wrapper.children().prop('data-hook')).toEqual(hookForRoot);
+      expect(wrapper.find(`[data-hook="${hookOfChild}"]`).exists()).toBe(true);
     });
   });
 });
